35f34bb08e659140ad056a68d71621fbd9f66917
unknown <nosutti1110@gmail.com>
2018-02-13 17:18:04 +0900

    kaidan no yatsu

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-a, b = 0,1
-while b<10 :
-    print(b)
-    a,b = b, a+b
+def stair(n):
+    string = ""
+    for i in range(n + 1):
+        string += str(i)
+        print(string)
+
+def stairr(n):
+    string = ""
+    for i in range(n + 1):
+        string += str(i)
+    for i in range(n + 1):
+        print(string[0:n + 1 - i])
+
+x = int(input("Please enter an integer: "))
+
+for i in range(x + 1):
+    stair(i)
+    stairr(i-1)
